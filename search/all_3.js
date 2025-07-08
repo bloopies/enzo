e56289d5b5fec5262f04ea3d70e9d10fb5fe2458
ParkerBritt <ParkerBritt@users.noreply.github.com>
2025-07-08 18:13:36 +0000

    deploy: 6f2a1ce5322f01b4340ac167b399714adfd11f87

--- search/all_3.js
+++ search/all_3.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['geometry_0',['Geometry',['../classenzo_1_1geo_1_1Geometry.html',1,'enzo::geo']]],
   ['geometryconnection_1',['GeometryConnection',['../classenzo_1_1nt_1_1GeometryConnection.html',1,'enzo::nt']]],
-  ['geometryoperator_2',['GeometryOperator',['../classenzo_1_1nt_1_1GeometryOperator.html',1,'enzo::nt']]]
+  ['geometryopdef_2',['GeometryOpDef',['../classenzo_1_1nt_1_1GeometryOpDef.html',1,'enzo::nt']]],
+  ['geometryoperator_3',['GeometryOperator',['../classenzo_1_1nt_1_1GeometryOperator.html',1,'enzo::nt']]]
 ];
