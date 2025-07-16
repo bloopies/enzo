41168188746179176f305c76d8e8c7d570316f9b
ParkerBritt <ParkerBritt@users.noreply.github.com>
2025-07-16 19:17:50 +0000

    deploy: c7b0b3f63dba909615ca3bb7905c5e63ff811549

--- search/all_4.js
+++ search/all_4.js
@@ -3,6 +3,5 @@ var searchData=
   ['geometry_0',['Geometry',['../classenzo_1_1geo_1_1Geometry.html',1,'enzo::geo']]],
   ['geometryconnection_1',['GeometryConnection',['../classenzo_1_1nt_1_1GeometryConnection.html',1,'enzo::nt']]],
   ['geometryopdef_2',['GeometryOpDef',['../classenzo_1_1nt_1_1GeometryOpDef.html',1,'enzo::nt']]],
-  ['geometryoperator_3',['GeometryOperator',['../classenzo_1_1nt_1_1GeometryOperator.html',1,'enzo::nt']]],
-  ['gop_5ftest_4',['GOP_test',['../classGOP__test.html',1,'']]]
+  ['geometryoperator_3',['GeometryOperator',['../classenzo_1_1nt_1_1GeometryOperator.html',1,'enzo::nt']]]
 ];
--- search/all_6.js
+++ search/all_6.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['operatortable_0',['OperatorTable',['../classenzo_1_1op_1_1OperatorTable.html',1,'enzo::op']]]
+  ['operatortable_0',['OperatorTable',['../classenzo_1_1op_1_1OperatorTable.html',1,'enzo::op']]],
+  ['opinfo_1',['OpInfo',['../structenzo_1_1op_1_1OpInfo.html',1,'enzo::op']]]
 ];
